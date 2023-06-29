77254debcb0fcba85788b54980a7ff4b124c140f
taras <regeidt.my@gmail.com>
2023-06-30 01:06:42 +0300

    spring_1 blogs/posts

--- src/middlewares/blogs-validation.ts
+++ src/middlewares/blogs-validation.ts
@@ -1,23 +1,47 @@
 import {withValidator} from "../utils/withValidator";
-import {body} from "express-validator";
+import {checkSchema} from "express-validator";
 
 export const blogsCreationValidator = withValidator(() => {
     return [
-        body("name")
-            .isString().withMessage("name should be a string")
-            .trim()
-            .isLength({min: 1}).withMessage("name should not be empty")
-            .isLength({max: 15}).withMessage("name length should be less than 15"),
-        body("description")
-            .isString().withMessage("description should be a string")
-            .trim()
-            .isLength({min: 1}).withMessage("description should not be empty")
-            .isLength({max: 500}).withMessage("description length should be less than 500"),
-        body("websiteUrl")
-            .isString().withMessage("websiteUrl should be a string")
-            .trim()
-            .isLength({min: 1}).withMessage("websiteUrl should not be empty")
-            .isLength({max: 100}).withMessage("websiteUrl length should be less than 100")
-            .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage("websiteUrl should match pattern 'https://'"),
+        checkSchema({
+            name: {
+                isString: {
+                    bail: true,
+                    errorMessage: "should be a string",
+                },
+                isLength: {
+                    options: { min: 1, max: 15 },
+                    errorMessage: "length should be > 0 < 15"
+                },
+            }
+        }),
+        checkSchema({
+            description: {
+                isString: {
+                    bail: true,
+                    errorMessage: "should be a string",
+                },
+                isLength: {
+                    options: { min: 1, max: 500 },
+                    errorMessage: "length should be > 0 < 500"
+                },
+            }
+        }),
+        checkSchema({
+            websiteUrl: {
+                isString: {
+                    bail: true,
+                    errorMessage: "should be a string",
+                },
+                isLength: {
+                    options: { min: 1, max: 100 },
+                    errorMessage: "length should be > 0 < 100"
+                },
+                matches: {
+                    options: /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/,
+                    errorMessage: "should match pattern 'https://'"
+                }
+            }
+        }),
     ]
 })
--- src/middlewares/posts-validation.ts
+++ src/middlewares/posts-validation.ts
@@ -1,26 +1,55 @@
 import {withValidator} from "../utils/withValidator";
-import {body} from "express-validator";
+import {checkSchema} from "express-validator";
 
 export const postsCreationValidator = withValidator(() => {
     return [
-        body("title")
-            .isString().withMessage("title should be a string")
-            .trim()
-            .isLength({min: 1}).withMessage("title should not be empty")
-            .isLength({max: 30}).withMessage("title length should be less than 30"),
-        body("shortDescription")
-            .isString().withMessage("shortDescription should be a string")
-            .trim()
-            .isLength({min: 1}).withMessage("shortDescription should not be empty")
-            .isLength({max: 100}).withMessage("shortDescription length should be less than 100"),
-        body("content")
-            .isString().withMessage("content should be a string")
-            .trim()
-            .isLength({min: 1}).withMessage("content should not be empty")
-            .isLength({max: 1000}).withMessage("content length should be less than 1000"),
-        body("blogId")
-            .isString().withMessage("blogId should be a string")
-            .trim()
-            .isLength({min: 1}).withMessage("blogId should not be empty")
+        checkSchema({
+            title: {
+                isString: {
+                    bail: true,
+                    errorMessage: "should be a string",
+                },
+                isLength: {
+                    options: { min: 1, max: 30 },
+                    errorMessage: "length should be > 0 < 30"
+                },
+            }
+        }),
+        checkSchema({
+            shortDescription: {
+                isString: {
+                    bail: true,
+                    errorMessage: "should be a string",
+                },
+                isLength: {
+                    options: { min: 1, max: 100 },
+                    errorMessage: "length should be > 0 < 100"
+                },
+            }
+        }),
+        checkSchema({
+            content: {
+                isString: {
+                    bail: true,
+                    errorMessage: "should be a string",
+                },
+                isLength: {
+                    options: { min: 1, max: 1000 },
+                    errorMessage: "length should be > 0 < 1000"
+                },
+            }
+        }),
+        checkSchema({
+            blogId: {
+                isString: {
+                    bail: true,
+                    errorMessage: "should be a string",
+                },
+                isLength: {
+                    options: { min: 1 },
+                    errorMessage: "length should be > 0"
+                },
+            }
+        }),
     ]
 })
